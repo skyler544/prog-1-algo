73652b3899795354e1c940292be4e6bc73cc8426
Skyler Mayfield <skyler544@gmail.com>
2022-03-25 20:24:00 +0100

    add lht header

---
+++ LinearHashTable.h
@@ -0,0 +1,23 @@
+#ifndef LINEARHASHTABLE_H_
+#define LINEARHASHTABLE_H_
+
+class LinearHashTable {
+private:
+  int t[10];
+  int n;
+  int q;
+  int d;
+
+public:
+  int *getT();
+  int getN();
+  int getQ();
+  int getD();
+  void setN(int newVal);
+  void setQ(int newVal);
+  void setD(int newVal);
+  void storeHashed(int entry);
+  int find(int x);
+};
+
+#endif // LINEARHASHTABLE_H_
--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main(int argc, char *argv[]) {
 
 
+
     return 0;
 }
