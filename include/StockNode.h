6907cbae681b0bca469bcb8e0cb9483a9e046910
Skyler Mayfield <skyler544@gmail.com>
2022-03-29 18:29:11 +0200

    preparing for csv input

--- include/StockNode.h
+++ include/StockNode.h
@@ -1,22 +1,23 @@
 #ifndef STOCKNODE_H_
 #define STOCKNODE_H_
 #include <string>
+#include <ctime>
 
 class StockNode {
 private:
-  std::string date;
-  std::string open;
-  std::string high;
-  std::string low;
-  std::string close;
-  std::string volume;
-  std::string adj_close;
+  std::tm date;
+  double open;
+  double high;
+  double low;
+  double close;
+  double adj_close;
+  int volume;
 
   StockNode *next;
 
 public:
-  StockNode(std::string d, std::string o, std::string h, std::string l,
-            std::string c, std::string v, std::string ac);
+  StockNode(std::tm d, double o, double h, double l,
+            double c, double ac, int v);
 
   void printStockNode();
 
