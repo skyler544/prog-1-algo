7a97f83506d7f7e8a54b2f6d5b2d52273ab0111d
Skyler Mayfield <skyler544@gmail.com>
2022-03-29 20:17:55 +0200

    start controller.cpp

---
+++ src/Controller.cpp
@@ -0,0 +1,10 @@
+#include "Controller.h"
+
+bool Controller::add() {return false;}
+bool Controller::del() {return false;}
+bool Controller::import() {return false;}
+bool Controller::search() {return false;}
+bool Controller::plot() {return false;}
+bool Controller::save() {return false;}
+bool Controller::load() {return false;}
+bool Controller::quit() {return false;}
