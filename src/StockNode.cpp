577f13a9bbcfd0dbe189c14c2417a71a6431a3d1
Skyler Mayfield <skyler544@gmail.com>
2022-03-29 18:29:25 +0200

    preparing for csv input

--- src/StockNode.cpp
+++ src/StockNode.cpp
@@ -1,8 +1,9 @@
 #include "StockNode.h"
 #include <iostream>
+#include <iomanip>
 
-StockNode::StockNode(std::string d, std::string o, std::string h, std::string l,
-                     std::string c, std::string v, std::string ac) {
+StockNode::StockNode(std::tm d, double o, double h, double l,
+                     double c, double ac, int v) {
   date = d;
   open = o;
   high = h;
@@ -16,13 +17,13 @@ StockNode::StockNode(std::string d, std::string o, std::string h, std::string l,
 
 void StockNode::printStockNode() {
   std::cout << "====================\n";
-  std::cout << "Date: " << date << "\n";
+  std::cout << "Date: " << std::put_time(&date, "%d.%m.%Y") << "\n";
   std::cout << "Open: " << open << "\n";
   std::cout << "High: " << high << "\n";
   std::cout << "Low: " << low << "\n";
   std::cout << "Close: " << close << "\n";
-  std::cout << "Volume: " << volume << "\n";
   std::cout << "Adjusted Close: " << adj_close << "\n";
+  std::cout << "Volume: " << volume << "\n";
 }
 
 StockNode *StockNode::getNext() { return next; }
